e65a8a1ab1d08c01fa1c6748605499f2563f7f3c
cmgalvangrajales <cmgalvangrajales@gmail.com>
2017-07-10 11:34:27 -0500

    index maxi v2

---
+++ public/jsx/strings/strings.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let text = [
+    {
+        section:'whoweare',
+        title:'Mision',
+        description: 'Promover, divulgar, formular, asesorar, ejecutar y controlar proyectos y campañas que protendan '+
+        'mejorar las condiciones de vida de la poblaciòn en cuanto a su vivienda, contando con un gran equipo humano que '+
+        ' nos permita brindar con responsabilidad, calidad, agilidad y cumplimiento en soluciones integrales del ramo de la'+
+        ' construcciòn de casas prefabricadas modulares de excelente estructuraciòn, contando con diversos diseños y modelos '+
+        'econòmicos, seguros, durables y garantizados.'
+    },
+    {
+        section:'whoweare',
+        title:'Vision',
+        description: 'El objetivo principal esta en desaroollar planes, programas y proyectos tendientes a la conservaciòn '+
+        'y recuperaciòn del medio ambiente en general, fomentar empleo en la àrea rural y urbana con la participaciòn de la '+
+        'comunidad en actividades de vivienda, para asì proporcionar el mejoramiento de las condiciones de vida de todos los '+
+        'seres humanos. \nConocedores de la situaciòn social de nuetra regiòn y ante la demanda constante de vivienda por parte '+
+        'de la poblaciòn decidimos iniciar un frente para llevar con todas las facilidades necesarias y suficientes a que muchas '+
+        'personas interesadas hagan sus sueños una realidad: TENER SU PROPIA CASA.'
+    },
+    {
+        section:'whoweare',
+        title:'Quienes Somos',
+        description: 'Somos una empresa encargada de producir soluciones sistematizadas de vivienda que racionalizan recursos, '+
+        'minimizan constos y agilizan el proceso constructivo, a travès de un sistema prefabricado modular que presenta alternativas '+
+        'arquitectònicas con tècnicas constructivas adecuadas, es un sistema abierto, versàtil, que reduce costos y tiempo. '+
+        'rompe con los mètodos de diseño y armado tradicionales. \n Nuestras viviendas acreditan su calidad cumpliendo con '+
+        'las normas establecidas en el articulo 20 decreto 1160 del año 2010, segùn lo establecido por la Comisiòn Asesora Permanente '+
+        ' para el Règimen de constructores Sismo resistentes.'
+    },
+    {
+        section:'services',
+        img:'casa1',
+        titulo:'VIVIENDA DE INTERES SOCIAL',
+        descripcion:'Por ser un sistema mas economico que el tradicional, podemos ofrecer soluciones de vivienda.'
+    },
+    {
+        section:'services',
+        img:'casa2',
+        titulo:'VIVIENDA EN TERRAZAS',
+        descripcion:'Es una soluciòn excelente para la construcciòn sobre terrazas ya que son livianas, rapidas en el montaje, se acomodan a cualquier area y son casas economicas.'
+    },
+    {
+        section:'services',
+        img:'casa3',
+        titulo:'VIVIENDA URBANA Y RURAL CASAS CAMPESTRES',
+        descripcion:'La soluciòn perfecta por su velocidad de construcciòn, imprevistos, errores en la construcciòn y obteniendo su casa en un corto tiempo. Es considerada la mejor opcion para la construcciòn de casas campestres.'
+    },
+    {
+        section:'advantage',
+        id:'1',
+        titulo:'INOXIDABLE',
+        descripcion:'Los materiales utilizados son resistentes a agentes externos como son; El agua, el sol, etc.',
+        img:'casa1',
+        pull:'pull-left'
+    },
+    {
+        section:'advantage',
+        id:'2',
+        titulo:'RAPIDEZ Y EFECTIVIDAD',
+        descripcion:'en el montaje con mano de obra calificada al contar con equipos especializados.',
+        img:'casa2',
+        pull:'pull-right'
+    },
+    {
+        section:'advantage',
+        id:'3',
+        titulo:'ECONOMICA',
+        descripcion:'usted se ahorraría hasta un 30% del dinero comparado con una construcción tradicional...',
+        img:'casa3',
+        pull:'pull-left'
+    }
+];
+
+export  {text};
